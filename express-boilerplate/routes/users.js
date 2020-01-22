8e40cef62c2ff64baaedcc0f8da6afb887651edd
Duy <vietduy1512@gmail.com>
2020-01-22 10:56:09 +0700

    Add more route

--- express-boilerplate/routes/users.js
+++ express-boilerplate/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('List of users');
+});
+
+router.get('/info', function(req, res, next) {
+  res.send('User info');
 });
 
 module.exports = router;
