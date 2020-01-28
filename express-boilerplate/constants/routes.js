8a639f280b1b6240d05f116bb6b82a57ce718669
Duy <vietduy1512@gmail.com>
2020-01-28 22:34:52 +0700

    Implement create author page

--- express-boilerplate/constants/routes.js
+++ express-boilerplate/constants/routes.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
   author_list: 'authors/author_list',
   author_detail: 'authors/author_detail',
+  author_form: 'authors/author_form',
   book_list: 'books/book_list',
   book_detail: 'books/book_detail',
   bookinstance_list: 'bookinstances/bookinstance_list',
