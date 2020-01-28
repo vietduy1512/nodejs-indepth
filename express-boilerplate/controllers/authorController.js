e8f10394eee0a479c31d5f3b534dc55b4e5c6f67
Duy <vietduy1512@gmail.com>
2020-01-28 19:29:23 +0700

    Implement author detail page
    - Add more confitional

--- express-boilerplate/controllers/authorController.js
+++ express-boilerplate/controllers/authorController.js
@@ -1,5 +1,7 @@
 
 var Author = require('../models/author');
+var Book = require('../models/book');
+var async = require('async');
 
 exports.author_list = function(req, res, next) {
 
@@ -13,8 +15,27 @@ exports.author_list = function(req, res, next) {
       });
 };
 
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = function(req, res, next) {
+
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+            .exec(callback)
+        },
+        authors_books: function(callback) {
+            Book.find({ 'author': req.params.id },'title summary')
+            .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.author == null) {
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('authors/author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
+
 };
 
 exports.author_create_get = function(req, res) {
